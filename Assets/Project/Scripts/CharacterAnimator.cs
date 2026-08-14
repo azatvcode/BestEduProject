66bddde51dc979113321fa05f91c40664f6aa085
azatvcode <97603marat@gmail.com>
2026-08-14 17:26:33 +0300

    animation and sound upgrade

--- Assets/Project/Scripts/CharacterAnimator.cs
+++ Assets/Project/Scripts/CharacterAnimator.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioSource;
     public void OnFootstep()
     {
-        
+        _audioSource.Play();
     }
 }
